0b8e53565844129d5aed1e2f9854a74da7291416
Jay Whitmore <jayraydon@gmail.com>
2021-08-11 11:38:12 +0700

    fix: combine news panel into one panel rather than two

--- src/components/NewsPromo.vue
+++ src/components/NewsPromo.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="w-full mb-25">
+  <div class="w-full">
     <h3>XE Distribution now open</h3>
-    <div class="rounded border border-black">
+    <div class="border border-black rounded-t">
       <img src="/assets/e-xe-promo.png">
     </div>
-    <div class="bg-white rounded mt-4 px-12 py-10">
+    <div class="px-12 py-10 bg-white rounded-b">
       <p>XE distribution is now open. If you haven't already done so, head to Edge Console and provide your XE wallet address.</p>
       <div class="w-full text-right">
-        <!-- <a href="https://edge.network/news/article/here" target="_about" class="button button--outline py-8 mr-8">More info</a> -->
-        <a href="https://console.edge.network" target="_about" class="button button--solid py-8">Edge Console</a>
+        <!-- <a href="https://edge.network/news/article/here" target="_about" class="py-8 mr-8 button button--outline">More info</a> -->
+        <a href="https://console.edge.network" target="_about" class="py-8 button button--solid">Edge Console</a>
       </div>
     </div>
   </div>

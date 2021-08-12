46ddd9885b4ef214a8a1b9f994ae57a3ac1c3924
Jay Whitmore <jayraydon@gmail.com>
2021-08-12 13:18:33 +0700

    feat: adjust summary screen newsPanel so it always matches height of statistics column

--- src/components/NewsPromo.vue
+++ src/components/NewsPromo.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="w-full">
+  <div class="flex flex-col w-full h-full">
     <h3>XE Distribution now open</h3>
     <div class="border border-black rounded-t">
       <img src="/assets/e-xe-promo.png">
     </div>
-    <div class="px-12 py-10 bg-white rounded-b">
-      <p>XE distribution is now open. If you haven't already done so, head to Edge Console and provide your XE wallet address.</p>
+    <div class="flex flex-col flex-1 px-12 py-10 bg-white rounded-b">
+      <p class="flex-1">XE distribution is now open. If you haven't already done so, head to Edge Console and provide your XE wallet address.</p>
       <div class="w-full text-right">
         <!-- <a href="https://edge.network/news/article/here" target="_about" class="py-8 mr-8 button button--outline">More info</a> -->
         <a href="https://console.edge.network" target="_about" class="py-8 button button--solid">Edge Console</a>

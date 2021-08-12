1fd876ea13339b1645148be61a88d0c9c9c2199b
Jay Whitmore <jayraydon@gmail.com>
2021-08-12 20:13:32 +0700

    fix: commit renamed components (oops)

---
+++ src/components/BlockOverview.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="flex flex-col h-full">
+    <h3>Block Overview</h3>
+    <div class="flex flex-col flex-1 space-y-2">
+      <div class="transactionRow">
+        <div class="transactionRow__label">Average Transaction</div>
+        <div class="transactionRow__value">{{ block.average }}</div>
+      </div>
+      <div class="transactionRow">
+        <div class="transactionRow__label">Confirmations</div>
+        <div class="transactionRow__value">{{ block.confirmations }}</div>
+      </div>
+      <div class="transactionRow">
+        <div class="transactionRow__label">Completed</div>
+        <div class="transactionRow__value">{{ block.completed }} at {{ block.timestamp }}</div>
+      </div>
+      <div class="transactionRow">
+        <div class="transactionRow__label">Transactions</div>
+        <div class="transactionRow__value">{{ block.transactions }}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "BlockOverview",
+  props: {
+    block: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .transactionRow {
+    @apply px-12 md:px-24 py-12 text-sm text-gray-300 bg-white rounded w-full grid grid-cols-12 items-center;
+  }
+  .transactionRow__label {
+    @apply col-span-5 md:col-span-4;
+  }
+  .transactionRow__value {
+    @apply font-mono col-span-7 md:col-span-8 text-black;
+  }
+</style>
---
+++ src/components/BlockSummary.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col h-full">
+    <h3>Block Summary</h3>
+    <div class="tile">
+      This block was mined on <span>{{ block.completed }}</span> at <span>{{ block.timestamp }}</span>. It currently has <span>{{ block.confirmations }} confirmations</span> on the XE blockchain. A total of <span>{{ block.transactions }} XE</span> were sent in the block with the average transaction being <span>{{ block.average }} XE</span>.
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "BlockSummary",
+  props: {
+    block: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tile {
+    @apply flex-1 p-12 md:p-24 text-sm text-gray-300 bg-white rounded md:pr-50;
+  }
+  .tile span {
+    @apply text-black font-medium;
+  }
+</style>

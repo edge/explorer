786af80acd7fc592e70f1c77c1be5848734edd90
Jay Whitmore <jayraydon@gmail.com>
2021-08-12 20:37:57 +0700

    refactor: adjust header panel height so it doesn't jump around between listing>detail screens

--- src/components/HeroPanel.vue
+++ src/components/HeroPanel.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="hero-panel">
-    <div class="container flex flex-col md:justify-between md:flex-row md:items-center">
+    <div class="container flex flex-col justify-end h-full md:justify-between md:flex-row md:items-end">
       <div class="flex flex-col">
-        <h1 class="max-w-lg truncate hero-panel__title">{{ title }}</h1>
-        <div class="max-w-lg mt-10 font-mono text-white truncate" :class="hash ? '' : 'hidden'">
+        <h1 class="hero-panel__title">{{ title }}</h1>
+        <div class="max-w-md mt-10 font-mono text-white truncate lg:max-w-lg" :class="hash ? '' : 'hidden'">
           <span class="text-green">Hash: </span>
           {{ hash }}
         </div>
@@ -35,7 +35,7 @@ export default {
 
 <style scoped>
 .hero-panel {
-  @apply bg-black-100 pb-20 pt-64;
+  @apply bg-black-100 pb-24 md:pb-20 h-180 md:h-128;
 }
 .hero-panel__title {
   @apply text-white text-2xl font-normal leading-none mb-0;

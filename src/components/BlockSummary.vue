6ce70eb2a091d21e47a2bfd63c7c8cc9573c3929
Jay Whitmore <jayraydon@gmail.com>
2021-08-19 10:32:50 +0700

    fix: remove rawData state from blockSummary

--- src/components/BlockSummary.vue
+++ src/components/BlockSummary.vue
@@ -2,15 +2,9 @@
   <div class="flex flex-col h-full">
     <div class="flex items-end justify-between mb-10">
       <h3 class="p-0 m-0">Block Summary</h3>
-      <button class="monospace" @click.prevent="rawDataVisible = !rawDataVisible">
-        {{ rawDataVisible ? 'Hide Raw Data' : 'View Raw Data' }}
-      </button>
     </div>
     <div class="relative max-h-full overflow-hidden tile">
-      <div v-if="rawDataVisible" class="top-0 left-0 h-full max-h-full overflow-auto lg:absolute lg:p-24">
-        {{ block }}
-      </div>
-      <div v-else class="md:pr-50">
+      <div class="md:pr-50">
         This block was mined on <span>{{new Date(block.timestamp).toLocaleString().split(',')[0]}}</span> at <span>{{new Date(block.timestamp).toLocaleString().split(',')[1]}}</span>. A total of <span>{{ block.total }} XE</span> were sent in the block with the average transaction being <span>{{ block.average }} XE</span>.
       </div>
     </div>
@@ -24,11 +18,6 @@ export default {
   props: {
     block: {
       type: Object
-    }
-  },
-  data: function () {
-    return {
-      rawDataVisible: false
     }
   }
 }

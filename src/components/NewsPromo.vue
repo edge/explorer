b6f3bd290ef63b278750f75062e4879493d135a5
Adam K Dean <adamkdean@googlemail.com>
2021-09-06 13:25:51 +0100

    feat: update news promo to wiki

--- src/components/NewsPromo.vue
+++ src/components/NewsPromo.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="flex flex-col w-full h-full">
-    <h3>XE Distribution is closing soon</h3>
+    <h3>Visit the Edge Community Wiki</h3>
     <div class="border border-black rounded-t">
-      <img src="/assets/e-xe-promo.png">
+      <img src="/assets/e-wiki-promo.png">
     </div>
     <div class="flex flex-col flex-1 px-12 py-10 bg-white rounded-b">
-      <p class="flex-1">The claim period for XE distribution closes September 4th. If you haven't already done so, head to Edge Console and provide your XE wallet address.</p>
+      <p class="flex-1">Learn about the technologies behind the platform, find out how to contribute to the network and read answers to common support questions and more.</p>
       <div class="w-full text-right">
         <!-- <a href="https://edge.network/news/article/here" target="_about" class="py-8 mr-8 button button--outline">More info</a> -->
-        <a href="https://console.edge.network" target="_about" class="py-8 button button--solid">Edge Console</a>
+        <a href="https://wiki.edge.network/" target="_about" class="py-8 button button--solid">Edge Wiki</a>
       </div>
     </div>
   </div>

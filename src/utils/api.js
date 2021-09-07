634f7bb9c42cb69eba1200cb03f0f404cb7e8e79
Adam K Dean <adamkdean@googlemail.com>
2021-09-07 14:07:39 +0100

    chore: update header comment with license

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,7 @@
+// Copyright (C) 2021 Edge Network Technologies Ltd
+// Use of this source code is governed by an MIT-style license that can be
+// found in the LICENSE file or at https://opensource.org/licenses/MIT.
+
 const qs = require('querystring')
 const {
   xeStringFromMicroXe
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+// Copyright (C) 2021 Edge Network Technologies Ltd
+// Use of this source code is governed by an MIT-style license that can be
+// found in the LICENSE file or at https://opensource.org/licenses/MIT.
+
 module.exports = {
   purge: [],
   darkMode: false,

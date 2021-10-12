c2330b1018f894b41bc7a041b02b8a2cb752d2e5
Jay Whitmore <jayraydon@gmail.com>
2021-10-12 22:34:06 +0700

    fix: remove space

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="relative z-10 py-16 header md:pb-15" :class="{'menu-open':showNav}">
       <div class="container flex items-center justify-between">
-        <Logo />
+        <Logo/>
         <BurgerButton @click="showNav = !showNav"/>
         <div id="menu" class="absolute left-0 right-0 flex flex-col flex-1 pt-12 pb-24 bg-black mobile-drop top-full md:static md:flex-row md:px-15 md:p-0">
           <Menu :mainNav="mainNav"/>

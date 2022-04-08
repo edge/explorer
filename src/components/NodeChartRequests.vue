def5f0a023b60e9741a1f8ea4de2549fe10d9dfe
Will Garrett <willgarrett64@yahoo.com>
2022-04-08 12:17:33 +0100

    feat: add node chart components

---
+++ src/components/NodeChartRequests.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex flex-col h-full">
+    <h3>Requests</h3>
+    <div class="relative max-h-full tile md:pr-50">
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "NodeChartRequests",
+  props: {
+    session: {
+      type: Object
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style scoped>
+  .tile {
+    @apply flex-1 p-12 md:p-24 text-sm text-gray-300 bg-white rounded;
+  }
+  .tile span.emphasis {
+    @apply text-gray-900 font-medium font-mono;
+  }
+  button {
+    @apply leading-none text-sm2 border-b border-black border-opacity-25 hover:border-green hover:border-opacity-25 hover:text-green align-middle;
+  }
+</style>

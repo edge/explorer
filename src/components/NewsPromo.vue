ab100ec9bae8a6013d02e1fd28fcad11d0b9bc3d
Will Garrett <willgarrett64@yahoo.com>
2023-01-27 14:42:06 +0000

    feat: promo image to burned tokens live image

--- src/components/NewsPromo.vue
+++ src/components/NewsPromo.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="flex flex-col w-full h-full">
     <h3 class="md:text-gray-100">Visit the Edge Community Wiki</h3>
-    <a href="https://wiki.edge.network/" class="rounded">
+    <router-link to="/burns" class="rounded">
+      <img src="/assets/burned-live.png" class="wiki-image w-full object-cover object-top rounded">
+    </router-link>
+    <!-- <a href="https://wiki.edge.network/" class="rounded">
       <img src="/assets/e-wiki-promo.png" class="wiki-image w-full object-cover object-top rounded">
-    </a>
+    </a> -->
     <!-- temporarily disabled the text and button to fit additional statistics onto the overview apge -->
     <!-- <div class="flex flex-col flex-1 px-12 pt-12 pb-12 bg-white rounded-b">
       <p class="flex-1">The Community Wiki is the go to resource for all things Edge. Learn about the tech, find out how to contribute, and get answers to common support questions.</p>
